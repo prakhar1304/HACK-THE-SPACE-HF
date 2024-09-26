8c84c7d0d03b31725990aa4b72ab9652f00128e5
PRAKHAR MADHARIA <155750280+prakhar1304@users.noreply.github.com>
2024-09-26 20:24:15 +0530

    second commit

--- App.tsx
+++ App.tsx
@@ -1,78 +1,22 @@
-import {
-  ConnectWallet,
-  embeddedWallet,
-  localWallet,
-  metamaskWallet,
-  rainbowWallet,
-  ThirdwebProvider,
-  trustWallet,
-  walletConnect,
-} from '@thirdweb-dev/react-native';
 import React from 'react';
-import {StyleSheet, Text, useColorScheme, View} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {TW_CLIENT_ID} from '@env';
+import { View } from 'react-native';
+import AppNavigation from './src/navigation/AppNavigator/AppNavigation';
+import { createNavigationContainerRef, NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import this
+
+export const navigateRef: any = createNavigationContainerRef();
 
 const App = () => {
   return (
-    <ThirdwebProvider
-      activeChain="mumbai"
-      clientId={TW_CLIENT_ID}
-      supportedWallets={[
-        metamaskWallet({
-          recommended: true,
-        }),
-        rainbowWallet(),
-        walletConnect({
-          recommended: true,
-        }),
-        embeddedWallet({
-          auth: {
-            // you need to enable EmbeddedWallets under your API Key in your thirdweb dashboard:
-            // https://thirdweb.com/dashboard/settings/api-keys
-            options: ['email', 'google'],
-            // you need to add this deeplink in your allowed `Redirect URIs` under your API Key in your thirdweb dashboard:
-            // https://thirdweb.com/dashboard/settings/api-keys
-            redirectUrl: 'rnstarter://',
-          },
-        }),
-        trustWallet(),
-        localWallet(),
-      ]}>
-      <AppInner />
-    </ThirdwebProvider>
+    // Wrap everything inside GestureHandlerRootView to enable gesture support
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer ref={navigateRef}>
+        <View style={{ flex: 1 }}>
+          <AppNavigation />
+        </View>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
-};
-
-const AppInner = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const textStyles = {
-    color: isDarkMode ? Colors.white : Colors.black,
-    ...styles.heading,
-  };
-
-  return (
-    <View style={styles.view}>
-      <Text style={textStyles}>React Native thirdweb starter</Text>
-      <ConnectWallet />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  view: {
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center',
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
+}
 
 export default App;
--- metro.config.js
+++ metro.config.js
@@ -8,10 +8,20 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const extraNodeModules = require('node-libs-browser');
 
+
+const defaultConfig = getDefaultConfig(__dirname);
+
 const config = {
   resolver: {
     extraNodeModules,
+
+    assetExts: [
+      ...defaultConfig.resolver.assetExts,
+      'obj', 'mtl', 'JPG', 'vrx', 'hdr', 'gltf', 'glb', 'bin', 'arobject', 'gif',
+    ], 
   },
+
+ 
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
